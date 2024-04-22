9e6ab2a68d86f75780153975a682317a8d6889da
Kağan Eriş <123362258+kaganeris@users.noreply.github.com>
2024-04-22 11:50:05 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] // Bunun bir Id olduğunu uygulamaya belirttik
+        public string CategoryID { get; set; } // MongoDB'de ID'ler string olarak tutulur.
+        public string CategoryName { get; set; }
+    }
+}

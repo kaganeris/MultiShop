64b11a0ef32b56f8182b8cebedca4a44f12d8bd8
Kağan Eriş <123362258+kaganeris@users.noreply.github.com>
2024-05-02 13:59:55 +0300

    d

--- Services/Basket/MultiShop.Basket/Controllers/BasketController.cs
+++ Services/Basket/MultiShop.Basket/Controllers/BasketController.cs
@@ -22,7 +22,6 @@ namespace MultiShop.Basket.Controllers
         [HttpGet]
         public async Task<IActionResult> GetMyBasketDetail()
         {
-            var user = User.Claims;
             var values = await _basketService.GetBasket(_loginService.GetUserId);
             return Ok(values);
         }

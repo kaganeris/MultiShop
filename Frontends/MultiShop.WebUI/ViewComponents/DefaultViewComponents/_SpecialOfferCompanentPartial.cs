a6e3726e9c8d653052dbe5026aaa051ff6e4e2fe
Kağan Eriş <123362258+kaganeris@users.noreply.github.com>
2024-05-06 13:21:33 +0300

    Frontend başlandı

---
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_SpecialOfferCompanentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _SpecialOfferCompanentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
